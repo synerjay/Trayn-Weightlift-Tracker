f028e7a5dcedd193b0f00a0c41b9c281a157292e
Jerome Tolentino <tolent.jay@gmail.com>
2021-02-25 19:45:21 +0900

    Added database connection and config files

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    // Await sync are always used with try catch curly braces
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+// this db.js file is used to connect mongoose and to prevent cluttering the server.js file
+module.exports = connectDB;
