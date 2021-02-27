c1593eedc9fbb914d13b669bceef361c40369dfc
Jerome Tolentino <tolent.jay@gmail.com>
2021-02-27 18:23:52 +0900

    Added user registration method

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const connectDB = async () => {
   try {
     await mongoose.connect(db, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      useCreateIndex: true
     });
     // Await sync are always used with try catch curly braces
     console.log('MongoDB Connected...');
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator/check');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
 
 // @router  POST api/users
 // @desc    Register user
@@ -17,13 +21,54 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     console.log(req.body);
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send('User route');
+
+    const { name, email, password } = req.body;
+
+    try {
+      // Step 1: See if user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+      // Step 2: Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200', // pic size
+        r: 'pg', // pg rating
+        d: 'mm' // default pic -- even with no gravatar
+      });
+
+      // New instance of user -- NOT yet saved:
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
+
+      // Step 3: Encrypt password using b-crypt
+      //Create a "salt"
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+      //Step 4: Return jsonwebtoken - logged in right away in the front end
+
+      return res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
