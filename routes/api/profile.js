2ab53f0891c808174a13c70fd01e13bc8cff76b0
Jerome Tolentino <tolent.jay@gmail.com>
2021-02-28 18:53:20 +0900

    Added POST method for profile file

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
@@ -24,5 +25,31 @@ router.get('/me', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+// @router  POST api/profile
+// @desc    Create or update user profile
+// @access  Private access with tokens
+
+//In this router post method, the "auth" middleware and the check express validator is used so they are put in an arrray
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+      check('skills', 'Skills is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
 
 module.exports = router;
