dc79704a629a2b36790da3783a285fa53b12c6cf
Jerome Tolentino <tolent.jay@gmail.com>
2021-02-28 13:38:30 +0900

    Added authorization token middleware and sending back user info object

---
+++ middleware/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+module.exports = function(req, res, next) {
+  // Get token from header of the request
+  const token = req.header('x-auth-token');
+
+  // Check if there are no token
+  if (!token) {
+    return res.status(401).json({ msg: 'No token, authorization denied' });
+  }
+
+  //Verify token
+
+  try {
+    const decoded = jwt.verify(token, config.get('jwtSecret'));
+
+    req.user = decoded.user; // user info is stored from the payload
+    next(); // middleware always have this next function
+  } catch (err) {
+    res.status(401).json({ msg: 'Token is not valid' });
+  }
+};
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+
+// Bring in the user model schema from Models folder to send user info
+const User = require('../../models/User');
 
 // @router  GET api/auth
 // @desc    Test route
 // @access  Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password'); // -password means that the password from user info isnt being returned
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
