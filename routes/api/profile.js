9d9290552be7bcecdd0c3c70130ede5a05af7e0e
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-01 18:12:40 +0900

    added user profile experience deletion

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -144,6 +144,7 @@ router.get('/user/:user_id', async (req, res) => {
     res.json(profile);
   } catch (err) {
     console.error(err.message);
+    // The purpose of err.kind == ObjectId is to not have Server Error if the user id params is too long or too short
     if (err.kind == 'ObjectId') {
       return res.status(400).json({ msg: 'Profile not found' });
     }
@@ -170,4 +171,96 @@ router.delete('/', auth, async (req, res) => {
   }
 });
 
+// @route PUT api/profile/experience
+// @desc Add profile experience
+// @access Private
+
+// For the Profile experience want to update a profile that's already stored, so a PUT method is used
+// auth middleware and check validator are all in the same area, so they are enclosed in brackets as shown below
+//checks are used to check the required fields are filled in
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+      check('company', 'Company is required')
+        .not()
+        .isEmpty(),
+      check('from', 'From date is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    //Check to see if there errors in fields not filling in
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    //Next step is to deconstruct the req.body to pull out the information in the request
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    // Make a new Object for the new Experiences
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    //Try and catch error to find profile using id from req.user (token)
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save(); // save to database
+
+      res.json(profile); // send a response with the profile object
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @route DELETE api/profile/experience/:exp_id
+// @desc Delete experience from profile
+// @access Private
+
+router.delete('/experience/:exp_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    //Get remove index
+    const removeIndex = profile.experience
+      .map(item => item.id)
+      .indexOf(req.params.exp_id);
+
+    profile.experience.splice(removeIndex, 1);
+
+    await profile.save();
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 module.exports = router;
