32ccb3e248ac8ab7a76c69fb664005d721122e35
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-03 18:17:53 +0900

    added post comments functionality

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -194,4 +194,90 @@ router.put('/unlike/:id', auth, async (req, res) => {
   }
 });
 
+// @router  Posting COMMENTS - POST api/posts/comment/:post_id
+// @desc    Comment on a post
+// @access  Private
+router.post(
+  '/comment/:id',
+  [
+    auth,
+    [
+      check('text', 'Text is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const post = await Post.findById(req.params.id)
+
+      const newComment = {
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id
+      };
+
+      post.comments.unshift(newComment);
+
+      await post.save(); // Save new post
+
+      res.json(post.comments);
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+// @router  DELETE api/posts/comment/:postid/:comment_id
+// @desc    Comment on a post
+// @access  Private
+
+router.delete('/comment/:id/:comment_id', auth, async (req, res)=> {
+ try {
+  const post = await Post.findById(req.params.id);
+
+  // If post id doesnt exist, then send 404 error
+  if (!post) {
+    return res.status(404).json({ msg: 'Post not found' });
+  }
+
+  //Pull out comment
+   const comment = post.comments.find(comment => comment.id === req.params.comment_id)
+
+   //Make sure comment exists
+   if(!comment) {
+     return res.status(404).json({ msg: 'Comment does not exist'})
+   }
+
+   // Check user
+
+   if(comment.user.toString() !== req.user.id) {
+     return res.status(400).json({ msg: 'User not authorized'})
+   }
+
+   //Get remove index
+   const removeIndex = post.comments
+      .map(comment => comment.user.toString())
+      .indexOf(req.user.id);
+
+    post.comments.splice(removeIndex, 1);
+
+    await post.save();
+
+    res.json(post.comments);
+
+ } catch (err) {
+   console.error(err.message);
+      res.status(500).send('Server Error');
+ }
+})
+
 module.exports = router;
