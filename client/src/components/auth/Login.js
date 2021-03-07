c9ee67a73b5ca3dc7df7135daad4b7f42b4f4559
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-07 14:07:22 +0900

    Updated the Login and Registration page, ready for backend connection and redux operations

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 export const Login = () => {
+  // Component State Hook
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  });
+
+  const { email, password} = formData;
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value})
+        // The ...formData is a spreader and copies the formData
+        // [e.target.name] corresponding to "name" attribute (not the value) of each HTML tags
+        // e.target.value -- is the change in value in the fields
+
+  //Subit function form
+  const onSubmit = e => {
+    e.preventDefault();
+      console.log('SUCCESS')
+  
+  }
+  
   return (
-    <div>
-      Login
-    </div>
+    <Fragment>
+     {/*<div className="alert alert-danger">
+        Invalid credentials 
+      </div>*/}
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Sign into Your Account</p>
+      <form className="form" onSubmit={e=> onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email} 
+            onChange={e => onChange(e)} 
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password} 
+            onChange={e => onChange(e)} 
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
   )
 }
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,115 @@
-import React from 'react'
+
+import React, { Fragment, useState } from 'react'
+import { Link } from 'react-router-dom';
 
 export const Register = () => {
+  // Component State Hook
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value})
+        // The ...formData is a spreader and copies the formData
+        // [e.target.name] corresponding to "name" attribute (not the value) of each HTML tags
+        // e.target.value -- is the change in value in the fields
+
+  //Subit function form
+  const onSubmit = e => {
+    e.preventDefault();
+    if(password !== password2) {
+      console.log('Passwords do not match')
+    } else {
+      console.log('SUCCESS')
+    }
+  }
+
+      // if we want to make a request to the server without using Redux
+      //Ultimately we want to use Redux to handle this request
+      // For references only below
+        /*
+            const newUser = {
+              name,
+              email,
+              password,
+              password2
+            }
+
+            try {
+              const config = {
+                headers: {
+                  'Content-Type': 'application/json'
+                }
+              }
+
+              const body = JSON.stringify(newUser);
+
+              const res = await axios.post('/api/users', body, config);
+              console.log(res.data)
+
+            } catch (err) {
+              console.error(err.response.data);
+            }
+          }
+          */
+
   return (
-    <div>
-      Register
-    </div>
+    <Fragment>
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead"><i className="fas fa-user"></i> Create Your Account</p>
+      <form className="form" onSubmit={e=> onSubmit(e)}>
+        <div className="form-group">
+          <input 
+            type="text" 
+            placeholder="Name" 
+            name="name" 
+            value={name} 
+            onChange={e => onChange(e)}
+            required 
+            />
+        </div>
+        <div className="form-group">
+          <input 
+            type="email" 
+            placeholder="Email Address" 
+            name="email"
+            value={email} 
+            onChange={e => onChange(e)} 
+            required
+            />
+          <small className="form-text"
+            >This site uses Gravatar so if you want a profile image, use a
+            Gravatar email</small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password} 
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2} 
+            onChange={e => onChange(e)}
+            minLength="6"
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to='/login'>Log In</Link>
+      </p>
+    </Fragment>
   )
 }
