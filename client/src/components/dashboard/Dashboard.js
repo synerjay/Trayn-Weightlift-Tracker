b9f9ef645c9e5324e7ecbc63e925abfc08f32fec
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-11 18:09:17 +0900

    Added getCurrentUsers Profile on the Dashboard component

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { getCurrentProfile } from '../../actions/profile';
 
-const Dashboard = (props) => {
+// We are going to use the getCurrentProfile action to redux as soon as the component loads
+// So we are going to use useEffect hooks to fire getCurrentProfile in the initial load
+
+const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+  useEffect(() => {
+    getCurrentProfile();
+  }, []); // <--- getCurrentProfil function is going to fire once
+
   return <div>Dashboard</div>;
 };
 
-Dashboard.propTypes = {};
+Dashboard.propTypes = {
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired,
+};
 
-export default Dashboard;
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  profile: state.profile,
+});
+
+export default connect(mapStateToProps, { getCurrentProfile })(Dashboard);
--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,3 +1,5 @@
+import { GET_PROFILE, PROFILE_ERROR } from '../actions/types';
+
 const initialState = {
   profile: null,
   profiles: [],
