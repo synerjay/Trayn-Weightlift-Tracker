e972aaeadf310c8aff492b8d9ff8c5373f2a3748
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-14 13:13:27 +0900

    Added ProfileTop component

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getProfileById } from '../../actions/profile';
 import { Link } from 'react-router-dom';
+import ProfileTop from './ProfileTop';
 
 // In this Profile component, to get the profile id, we need to get it from the params of the URL
 // In REACT, we can get the params of the component by accessing the 'match' object available from the component props
@@ -17,7 +18,7 @@ const Profile = ({
   // useEffect Hook to fire the getProfileById action to the redux and get the profile by params
   useEffect(() => {
     getProfileById(match.params.id); // in react, we get the params by accessing the match fobject from the props
-  }, [getProfileById]);
+  }, [getProfileById, match.params.id]); // <-- depedency arrays have the Action and the match.params
 
   return (
     <Fragment>
@@ -36,6 +37,11 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
+          <div class='profile-grid my-1'>
+            {' '}
+            {/* This div grid is needed to organize the profile components into a grid*/}
+            <ProfileTop profile={profile} />
+          </div>
         </Fragment>
       )}
     </Fragment>
