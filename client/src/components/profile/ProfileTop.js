8899242a99633e14bbe5153b6394fbecdd0f80df
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-14 13:35:20 +0900

    Finished ProfileTop component with dynamic links to social media pages

--- client/src/components/profile/ProfileTop.js
+++ client/src/components/profile/ProfileTop.js
@@ -20,24 +20,36 @@ const ProfileTop = ({
       </p>
       <p>{location && <span>{location}</span>}</p>
       <div className='icons my-1'>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i className='fas fa-globe fa-2x'></i>
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i className='fab fa-twitter fa-2x'></i>
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i className='fab fa-facebook fa-2x'></i>
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i className='fab fa-linkedin fa-2x'></i>
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i className='fab fa-youtube fa-2x'></i>
-        </a>
-        <a href='#' target='_blank' rel='noopener noreferrer'>
-          <i className='fab fa-instagram fa-2x'></i>
-        </a>
+        {website && (
+          <a href={website} target='_blank' rel='noopener noreferrer'>
+            <i className='fas fa-globe fa-2x'></i>
+          </a>
+        )}
+        {social && social.twitter && (
+          <a href={social.twitter} target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-twitter fa-2x'></i>
+          </a>
+        )}
+        {social && social.facebook && (
+          <a href={social.facebook} target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-facebook fa-2x'></i>
+          </a>
+        )}
+        {social && social.linkedin && (
+          <a href={social.linkedin} target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-linkedin fa-2x'></i>
+          </a>
+        )}
+        {social && social.youtube && (
+          <a href={social.youtube} target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-youtube fa-2x'></i>
+          </a>
+        )}
+        {social && social.instagram && (
+          <a href={social.instagram} target='_blank' rel='noopener noreferrer'>
+            <i className='fab fa-instagram fa-2x'></i>
+          </a>
+        )}
       </div>
     </div>
   );
