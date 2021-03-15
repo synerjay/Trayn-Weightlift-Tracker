ca59d849095e01a1ee687157a648c4d3f0e45bdd
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-15 16:33:29 +0900

    Created Post reducer functionality

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import alert from './alert';
 import auth from './auth';
 import profile from './profile';
+import post from './post';
 
 export default combineReducers({
   alert,
   auth,
   profile,
+  post,
 });
 
 //This is the root reducer. and this reducer combines all separate reducers into one
---
+++ client/src/reducers/post.js
@@ -0,0 +1,32 @@
+// Work flow:
+// If you want to add something to your app that requires state changes, you 1. create a new reducer, 2. a new action file, 3. new component
+
+import { GET_POSTS, POST_ERROR } from '../actions/types';
+
+const initialState = {
+  posts: [],
+  post: null,
+  loading: true,
+  error: {},
+};
+
+export default function (state = initialState, action) {
+  const { type, payload } = action;
+
+  switch (type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: payload,
+        loading: false,
+      };
+    case POST_ERROR:
+      return {
+        ...state,
+        error: payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
