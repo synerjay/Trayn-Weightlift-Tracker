764e5465eb8a9d619cdebf9bf02d24e92a2c3fa2
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-17 15:24:55 +0900

    Reorganized App.js component and transferred switch routes to separate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,18 @@
 import { Fragment, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-import Login from './components/auth/Login';
-import Register from './components/auth/Register';
+
 import Landing from './components/layout/Landing';
 import Navbar from './components/layout/Navbar';
+// put routes component here:
+
 //Redux Stuff
 import { Provider } from 'react-redux';
 import store from './store';
-import Alert from './components/layout/Alert';
 import { loadUser } from './actions/auth';
 import setAuthToken from './utils/setAuthToken';
-import Dashboard from './components/dashboard/Dashboard';
-import PrivateRoute from './components/routing/PrivateRoute';
-import AddExperience from './components/profile-forms/AddExperience';
-import ProfileForm from './components/profile-forms/ProfileForm';
-import AddEducation from './components/profile-forms/AddEducation';
-import Profiles from './components/profiles/Profiles';
-import Profile from './components/profile/Profile';
-import Posts from './components/posts/Posts';
-import Post from './components/post/Post';
 import { LOGOUT } from './actions/types';
+import Routes from './components/routing/Routes';
 
 //Provider tag provides the components with the props that will be the redux states
 //This connects React and Redux together
@@ -50,40 +42,7 @@ const App = () => {
         <Fragment>
           <Navbar />
           <Route exact path='/' component={Landing} />
-          <section className='container'>
-            {' '}
-            {/* This section is for the Login and Register components to center in the page*/}
-            <Alert />
-            <Switch>
-              <Route exact path='/register' component={Register} />
-              <Route exact path='/login' component={Login} />
-              <Route exact path='/profiles' component={Profiles} />
-              <Route exact path='/profile/:id' component={Profile} />
-              <PrivateRoute exact path='/dashboard' component={Dashboard} />
-              <PrivateRoute
-                exact
-                path='/create-profile'
-                component={ProfileForm}
-              />
-              <PrivateRoute
-                exact
-                path='/edit-profile'
-                component={ProfileForm}
-              />
-              <PrivateRoute
-                exact
-                path='/add-experience'
-                component={AddExperience}
-              />
-              <PrivateRoute
-                exact
-                path='/add-education'
-                component={AddEducation}
-              />
-              <PrivateRoute exact path='/posts' component={Posts} />
-              <PrivateRoute exact path='/posts/:id' component={Post} />
-            </Switch>
-          </section>
+          <Route component={Routes} />
         </Fragment>
       </Router>
     </Provider>
