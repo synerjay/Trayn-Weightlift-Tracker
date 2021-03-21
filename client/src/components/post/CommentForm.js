a28e05fc3ed69d850fb744b55ecdb6cf181278b4
Jerome Tolentino <tolent.jay@gmail.com>
2021-03-21 18:30:17 +0900

    Added Material UI style to Comment Form - Testin phase

--- client/src/components/post/CommentForm.js
+++ client/src/components/post/CommentForm.js
@@ -3,10 +3,20 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { addComment } from '../../actions/post';
 import Button from '@material-ui/core/Button';
-import { TextField } from '@material-ui/core';
+import { makeStyles, TextField } from '@material-ui/core';
 import KeyboardArrowRightOutlinedIcon from '@material-ui/icons/KeyboardArrowRightOutlined';
 
+const useStyles = makeStyles({
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: 'block',
+  },
+});
+
 const CommentForm = ({ postId, addComment }) => {
+  const classes = useStyles();
+
   const [text, setText] = useState('');
 
   const onSubmit = (e) => {
@@ -20,9 +30,14 @@ const CommentForm = ({ postId, addComment }) => {
       <div className='bg-primary p'>
         <h3>Leave A Comment</h3>
       </div>
-      <form className='form my-1' onSubmit={onSubmit}>
+      <form
+        // className='form my-1'
+        onSubmit={onSubmit}
+      >
         <TextField
+          className={classes.field}
           name='text'
+          label='Comment'
           value={text}
           variant='outlined'
           onChange={(e) => setText(e.target.value)}
