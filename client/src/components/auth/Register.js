0a16bbd763a46767b1e3be7fda94ff55214454b7
Jerome Tolentino <tolent.jay@gmail.com>
2021-04-26 12:07:54 +0900

    Trying styling the registration page

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -68,60 +68,65 @@ const Register = ({ setAlert, register, isAuthenticated }) => {
 
   return (
     <Fragment>
-      <h1 className='large text-primary'>Sign Up</h1>
-      <p className='lead'>
-        <i className='fas fa-user'></i> Create Your Account
-      </p>
-      <form className='form' onSubmit={(e) => onSubmit(e)}>
-        <div className='form-group'>
-          <input
-            type='text'
-            placeholder='Name'
-            name='name'
-            value={name}
-            onChange={(e) => onChange(e)}
-            required
-          />
+      <div className='flex justify-between w-auto border-gray-900 border-4'>
+        <div className='bg-gray-500 w-1/2 rounded-3xl border-gray-900 border-4'></div>
+        <div className='shadow p-10 w-3/6'>
+          <h1 className='large text-primary'>Sign Up</h1>
+          <p className='lead'>
+            <i className='fas fa-user'></i> Create Your Account
+          </p>
+          <form className='form' onSubmit={(e) => onSubmit(e)}>
+            <div className='form-group'>
+              <input
+                type='text'
+                placeholder='Name'
+                name='name'
+                value={name}
+                onChange={(e) => onChange(e)}
+                required
+              />
+            </div>
+            <div className='form-group'>
+              <input
+                type='email'
+                placeholder='Email Address'
+                name='email'
+                value={email}
+                onChange={(e) => onChange(e)}
+                required
+              />
+              <small className='form-text'>
+                This site uses Gravatar so if you want a profile image, use a
+                Gravatar email
+              </small>
+            </div>
+            <div className='form-group'>
+              <input
+                type='password'
+                placeholder='Password'
+                name='password'
+                value={password}
+                onChange={(e) => onChange(e)}
+                minLength='6'
+              />
+            </div>
+            <div className='form-group'>
+              <input
+                type='password'
+                placeholder='Confirm Password'
+                name='password2'
+                value={password2}
+                onChange={(e) => onChange(e)}
+                minLength='6'
+              />
+            </div>
+            <input type='submit' className='btn btn-primary' value='Register' />
+          </form>
+          <p className='my-1'>
+            Already have an account? <Link to='/login'>Log In</Link>
+          </p>
         </div>
-        <div className='form-group'>
-          <input
-            type='email'
-            placeholder='Email Address'
-            name='email'
-            value={email}
-            onChange={(e) => onChange(e)}
-            required
-          />
-          <small className='form-text'>
-            This site uses Gravatar so if you want a profile image, use a
-            Gravatar email
-          </small>
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Password'
-            name='password'
-            value={password}
-            onChange={(e) => onChange(e)}
-            minLength='6'
-          />
-        </div>
-        <div className='form-group'>
-          <input
-            type='password'
-            placeholder='Confirm Password'
-            name='password2'
-            value={password2}
-            onChange={(e) => onChange(e)}
-            minLength='6'
-          />
-        </div>
-        <input type='submit' className='btn btn-primary' value='Register' />
-      </form>
-      <p className='my-1'>
-        Already have an account? <Link to='/login'>Log In</Link>
-      </p>
+      </div>
     </Fragment>
   );
 };
