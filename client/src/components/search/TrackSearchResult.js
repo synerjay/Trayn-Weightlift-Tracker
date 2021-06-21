65fca316fe7a221b47866f503832a8dc5e0a4b31
Jerome Tolentino <tolent.jay@gmail.com>
2021-06-21 20:26:39 +0900

    so far so good

--- client/src/components/search/TrackSearchResult.js
+++ client/src/components/search/TrackSearchResult.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function TrackSearchResult({ track }) {
   return (
-    <div className='flex  items-center'>
+    <div className='flex items-center'>
       <img src={track.albumUrl} className='h-16 w-16' />
       <div className='ml-3'>
         <div>{track.title}</div>
