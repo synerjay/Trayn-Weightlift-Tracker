2f979af0f273a1edeaf733f144b318b6a66cd38f
Jerome Tolentino <tolent.jay@gmail.com>
2021-06-21 20:16:05 +0900

    So frickin awesome

--- client/src/components/search/SearchSpot.js
+++ client/src/components/search/SearchSpot.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import SpotifyWebApi from 'spotify-web-api-node';
+import TrackSearchResult from './TrackSearchResult';
 
 function SearchSpot() {
   const [token, setToken] = useState('');
@@ -9,43 +10,66 @@ function SearchSpot() {
 
   // This compoennt is for testing the search Artists Tracks function
   const { REACT_APP_CLIENT_ID, REACT_APP_CLIENT_SECRET } = process.env;
-
+  // sometimes as planned
   // New instance of the SpotifyWebApi
   const spotifyApi = new SpotifyWebApi();
 
   // Testing spotify-web-api node
 
   useEffect(() => {
-    // spotifyApi.setAccessToken(token);
-    spotifyApi.searchTracks('Humble Kendrick Lamar').then(
+    console.log(search);
+  }, [search]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    spotifyApi.setAccessToken(token); // just to make sure because sometimes searchTracks doesnt work
+    spotifyApi.searchTracks(search, { limit: 5 }).then(
       function (data) {
-        console.log('Search by "Love"', data.body);
+        setSearchResults(
+          data.body.tracks.items.map((track) => {
+            const smallestAlbumImage = track.album.images.reduce(
+              (smallest, image) => {
+                if (image.height < smallest.height) return image;
+                return smallest;
+              },
+              track.album.images[0]
+            );
+
+            return {
+              artist: track.artists[0].name,
+              title: track.name,
+              uri: track.uri,
+              albumUrl: smallestAlbumImage.url,
+              album: track.album.name,
+            };
+          })
+        );
       },
       function (err) {
         console.error(err);
       }
     );
 
-    spotifyApi.searchTracks('track:Alright artist:Kendrick Lamar').then(
-      function (data) {
-        console.log(
-          'Search tracks by "Alright" in the track name and "Kendrick Lamar" in the artist name',
-          data.body
-        );
-      },
-      function (err) {
-        console.log('Something went wrong!', err);
-      }
-    );
-    spotifyApi.searchArtists('doja cat').then(
-      function (data) {
-        console.log('Search artist', data.body);
-      },
-      function (err) {
-        console.error(err);
-      }
-    );
-  }, [token]);
+    // spotifyApi.searchTracks('track:Alright artist:Kendrick Lamar').then(
+    //   function (data) {
+    //     console.log(
+    //       'Search tracks by "Alright" in the track name and "Kendrick Lamar" in the artist name',
+    //       data.body
+    //     );
+    //   },
+    //   function (err) {
+    //     console.log('Something went wrong!', err);
+    //   }
+    // );
+    // spotifyApi.searchArtists('doja cat').then(
+    //   function (data) {
+    //     console.log('Search artist', data.body);
+    //   },
+    //   function (err) {
+    //     console.error(err);
+    //   }
+    // );
+  }, [search, token]);
 
   // Make a call to Spotify api using axios
   useEffect(() => {
@@ -67,9 +91,9 @@ function SearchSpot() {
 
   //REFERENCE on how to get SEARCH going: https://github.com/WebDevSimplified/spotify-clone/blob/main/client/src/Dashboard.js
   return (
-    <div className='h-80 w-auto'>
-      <form onSubmit={() => {}}>
-        <div className='h-auto inline-flex '>
+    <div className='flex justify-center items-center mt-5 h-screen w-screen'>
+      <form value={search} onChange={(e) => setSearch(e.target.value)}>
+        <div className='h-auto flex flex-row '>
           <div>
             {' '}
             <i className='fa fa-search text-gray-400 z-20 hover:text-gray-500'></i>{' '}
@@ -81,9 +105,11 @@ function SearchSpot() {
           />
           <div>
             {' '}
-            <button className='h-10 w-20 text-white rounded-lg bg-red-500 hover:bg-red-600'>
-              Search
-            </button>{' '}
+            <div className='overflow-scroll'>
+              {searchResults.map((track) => (
+                <TrackSearchResult track={track} key={track.uri} />
+              ))}
+            </div>
           </div>
         </div>
       </form>
