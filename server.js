69a8bc320381b32dc1036836e4641bbe580f33c9
Jerome Tolentino <tolent.jay@gmail.com>
2021-07-31 22:27:17 +0900

    made a new route specific to workout

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get('/', (req, res) => res.send('API is now running'));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/workout', require('./routes/api/workout'));
 app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
