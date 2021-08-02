42f4cd78dfd892703b503e87686ec16408b2fa9e
Jerome Tolentino <tolent.jay@gmail.com>
2021-08-02 18:00:43 +0900

    added workout actions types

---
+++ client/src/actions/workout.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+import { setAlert } from './alert';
+
+import {
+  GET_WORKOUT,
+  UPDATE_WORKOUT,
+  GET_WORKOUTS,
+  WORKOUT_ERROR,
+  CLEAR_WORKOUT,
+} from './types';
