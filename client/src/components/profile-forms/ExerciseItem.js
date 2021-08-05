3ae6c4d7fdbe4c1e1d90e04eca8689a5ac564987
Jerome Tolentino <tolent.jay@gmail.com>
2021-08-05 19:40:43 +0900

    styling changes to sets

--- client/src/components/profile-forms/ExerciseItem.js
+++ client/src/components/profile-forms/ExerciseItem.js
@@ -69,11 +69,23 @@ const ExerciseItem = ({
       </table>
       <div className='button-section'>
         <button
-          className='w-full bg-gray-300'
+          className='w-full h-5 bg-gray-300 flex justify-center items-center max-w-xs mx-auto hover:bg-gray-200 focus:bg-gray-200 text-gray-700 rounded-lg p-1 font-semibold'
           type='button'
           onClick={() => addFormFields()}
         >
-          Add Set
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-3 w-3 mt-0 mr-2'
+            viewBox='0 0 20 20'
+            fill='currentColor'
+          >
+            <path
+              fillRule='evenodd'
+              d='M15.707 4.293a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 011.414-1.414L10 8.586l4.293-4.293a1 1 0 011.414 0zm0 6a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 111.414-1.414L10 14.586l4.293-4.293a1 1 0 011.414 0z'
+              clipRule='evenodd'
+            />
+          </svg>{' '}
+          ADD SET
         </button>
         {/* <button className='button submit' type='submit'>
             Save
