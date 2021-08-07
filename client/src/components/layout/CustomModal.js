edc8e128d944af376c627bcbe6cc14a4cfc9b0a4
Jerome Tolentino <tolent.jay@gmail.com>
2021-08-07 15:57:17 +0900

    added styling to custom modals

--- client/src/components/layout/CustomModal.js
+++ client/src/components/layout/CustomModal.js
@@ -1,14 +1,60 @@
 import React, { useState } from 'react';
+import { format } from 'date-fns';
+import { Link } from 'react-router-dom';
 
 const CustomModal = ({ component: Component, setShowModal, workoutId }) => {
+  const [workoutHeader, setWorkoutHeader] = useState(null);
+
   return (
     <>
       <div className='h-full w-full justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
         <div className='h-5/6 w-5/6 overflow-y-autorelative my-6 mx-auto max-w-3xl'>
           {/*content*/}
           <div className=' h-full border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+            {/*header*/}
+            <div className='flex items-start justify-between p-5 border-b border-solid border-blueGray-200 rounded-t'>
+              <div className='flex flex-col'>
+                <h3 className='text-3xl font-semibold'>
+                  {workoutHeader && workoutHeader.workoutName}
+                </h3>
+                {workoutHeader && (
+                  <p className='text-lg'>
+                    {' '}
+                    Workout performed on{' '}
+                    {format(new Date(workoutHeader.date), 'yyyy/MM/dd')}
+                  </p>
+                )}
+              </div>
+              <div className='flex flex-row'>
+                <Link className='btn btn-light my-1' to='/add-exercise'>
+                  Edit Workout
+                </Link>
+                <button
+                  className='p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
+                  onClick={() => setShowModal(false)}
+                >
+                  <svg
+                    xmlns='http://www.w3.org/2000/svg'
+                    className='h-6 w-6'
+                    fill='none'
+                    viewBox='0 0 24 24'
+                    stroke='currentColor'
+                  >
+                    <path
+                      strokeLinecap='round'
+                      strokeLinejoin='round'
+                      strokeWidth={2}
+                      d='M6 18L18 6M6 6l12 12'
+                    />
+                  </svg>
+                </button>
+              </div>
+            </div>
             <div className='h-full w-full relative p-6 flex-auto overflow-scroll'>
-              <Component workoutId={workoutId} />
+              <Component
+                setWorkoutHeader={setWorkoutHeader}
+                workoutId={workoutId}
+              />
             </div>
             {/*footer*/}
             <div className=' h-px bg-opacity-0 bg-transparent flex items-center justify-end p-6 rounded-b'>
--- client/src/components/profile/WorkoutProfile.js
+++ client/src/components/profile/WorkoutProfile.js
@@ -4,17 +4,21 @@ import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
 import { getWorkout } from '../../actions/workout';
 import { Link } from 'react-router-dom';
-import { format } from 'date-fns';
 import ExerciseProfile from './ExerciseProfile';
 
 const WorkoutProfile = ({
   getWorkout,
   workout: { workout, loading },
   workoutId,
+  setWorkoutHeader,
 }) => {
   useEffect(() => {
     getWorkout(workoutId); // in react, we get the params by accessing the match object from the props
   }, [getWorkout, workoutId]);
+
+  useEffect(() => {
+    setWorkoutHeader(workout);
+  }, [workout]);
 
   return (
     <>
@@ -22,19 +26,11 @@ const WorkoutProfile = ({
         <Spinner />
       ) : (
         <div>
-          <h2 className='text-3xl'> {workout.workoutName}</h2>
-          <p className='text-lg'>
-            {' '}
-            Workout performed on {format(new Date(workout.date), 'yyyy/MM/dd')}
-          </p>
           <div className='w-full flex flex-col md:grid md:grid-cols-2 md:gap-5 p-5'>
             {workout.exercise.map((exercise) => (
               <ExerciseProfile key={exercise._id} exercise={exercise} />
             ))}
           </div>
-          <Link className='btn btn-light my-1' to='/add-exercise'>
-            Edit Workout
-          </Link>
         </div>
       )}
     </>
